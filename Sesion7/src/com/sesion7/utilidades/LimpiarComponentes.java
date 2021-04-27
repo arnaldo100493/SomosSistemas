b0a6d10ec26154e8babb3f42b46b6db4c4a47461
Arnaldo Andrés Barrios Mena <abarrios@cobiscorp.com>
2021-04-27 11:30:22 -0500

    Primera Subida del Proyecto.

---
+++ Sesion7/src/com/sesion7/utilidades/LimpiarComponentes.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sesion7.utilidades;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author FABAME
+ */
+public class LimpiarComponentes {
+
+    public LimpiarComponentes() {
+
+    }
+
+    public static void limpiarComponentes(JPanel panelDatos) {
+
+        Component[] listadoComponentes = panelDatos.getComponents();
+
+        for (Component item : listadoComponentes) {
+
+            if (item.getClass().getName().equals("javax.swing.JCheckBox")) {
+                ((javax.swing.JCheckBox) item).setSelected(false);
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JComboBox")) {
+                ((javax.swing.JComboBox) item).setSelectedIndex(0);
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JPasswordField")) {
+                ((javax.swing.JPasswordField) item).setText("");
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JRadioButton")) {
+                ((javax.swing.JRadioButton) item).setSelected(false);
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JTextArea")) {
+                ((javax.swing.JTextArea) item).setText("");
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JTextField")) {
+                ((javax.swing.JTextField) item).setText("");
+            }
+
+            if (item.getClass().getName().equals("javax.swing.JTextPane")) {
+                ((javax.swing.JTextPane) item).setText("");
+            }
+        }
+    }
+}
